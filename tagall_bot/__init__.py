b5b078aa2bb28f599e1f82d01f8fe993209019df
ultar1-sug <139182972+ultar1-sug@users.noreply.github.com>
2024-03-04 03:08:56 +0100

    Update __init__.py

--- tagall_bot/__init__.py
+++ tagall_bot/__init__.py
@@ -3,67 +3,49 @@
 import logging
 from os import environ
 
-from ptbcontrib.roles import Roles, setup_roles
 from telegram.ext import Dispatcher, Updater
-
 from tagall_bot.sql.roles import get_users, sudo_users, tag_users
 
-# enable logging
+# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
 LOGGER = logging.getLogger(__name__)
 
 TOKEN = environ.get("TOKEN", "")
-
-try:
-    OWNER_ID = int(environ.get("OWNER_ID", ""))
-except ValueError:
-    raise ValueError("Your OWNER_ID env variable is not a valid integer.")
-
-try:
-    SUDO_USERS = {int(x) for x in environ.get("SUDO_USERS", "").split()}
-except ValueError:
-    raise ValueError("Your sudo users list does not contain valid integers.")
-
-try:
-    SUDO_GROUPS = {int(x) for x in environ.get("SUDO_GROUPS", "").split()}
-except ValueError:
-    raise ValueError("Your sudo groups list does not contain valid integers.")
-
-try:
-    DND_USERS = {int(x) for x in environ.get("DND_USERS", "").split()}
-except ValueError:
-    raise ValueError("Your DND users list does not contain valid integers.")
-
+OWNER_ID = int(environ.get("OWNER_ID", ""))
+SUDO_USERS = {int(x) for x in environ.get("SUDO_USERS", "").split()}
+SUDO_GROUPS = {int(x) for x in environ.get("SUDO_GROUPS", "").split()}
+DND_USERS = {int(x) for x in environ.get("DND_USERS", "").split()}
 WEBHOOK = bool(environ.get("WEBHOOK", False))
-URL = environ.get("URL", "")  # Does not contain token
-API_URL = environ.get("API_URL", "")  # No trailing slash
+URL = environ.get("URL", "")
+API_URL = environ.get("API_URL", "")
 PORT = int(environ.get("PORT", 5000))
-SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.update(get_users(sudo_users), SUDO_GROUPS)
-TAG_USERS = get_users(tag_users)
 
-UPDATER: Updater = Updater(token=TOKEN)
-DISPATCHER: Dispatcher = UPDATER.dispatcher
-roles: Roles = setup_roles(DISPATCHER)
-roles.add_admin(OWNER_ID)
+# Set up Updater and Dispatcher
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
 
-if "TAG_USERS" not in roles:
-    roles.add_role("TAG_USERS")
+# Define roles
+sudo_users.update(get_users(sudo_users), SUDO_GROUPS)
+tag_users = get_users(tag_users)
 
-if "SUDO_USERS" not in roles:
-    roles.add_role("SUDO_USERS")
+# Set up roles
+for sudo in sudo_users:
+    dispatcher.bot_data.setdefault("sudo_users", set()).add(sudo)
+for tag in tag_users:
+    dispatcher.bot_data.setdefault("tag_users", set()).add(tag)
 
-SUDO_USERS = roles["SUDO_USERS"]
-TAG_USERS = roles["TAG_USERS"]
-SUDO_USERS.add_child_role(TAG_USERS)
-ADMINS = roles.admins
+# Additional setup for admin
+dispatcher.bot_data["owner_id"] = OWNER_ID
+dispatcher.bot_data["admins"] = sudo_users
 
-for sudo in get_users(sudo_users):
-    SUDO_USERS.add_member(sudo)
+# Additional setup for webhook
+if WEBHOOK:
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    updater.bot.set_webhook(URL + TOKEN)
+else:
+    updater.start_polling()
 
-for tag in get_users(tag_users):
-    TAG_USERS.add_member(tag)
+# Add any additional handlers or setup as needed
